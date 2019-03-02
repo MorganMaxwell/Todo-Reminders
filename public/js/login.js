b2bc992cf728b0e322f1a9d8ef7d2f65b49b0825
ibaveda <igorbaveda@gmail.com>
2019-03-02 12:57:48 -0700

    html files changed to handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -22,17 +22,18 @@ $(document).ready(function() {
     passwordInput.val("");
   });
 
-  // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+  // loginUser does a post to our "api/login" route and if successful, redirects us the the index page
   function loginUser(email, password) {
     $.post("/api/login", {
       email: email,
       password: password
-    }).then(function(data) {
-      window.location.replace(data);
-      // If there's an error, log the error
-    }).catch(function(err) {
-      console.log(err);
-    });
+    })
+      .then(function(data) {
+        window.location.replace(data);
+        // If there's an error, log the error
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
   }
-
 });
