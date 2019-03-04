863918604ffab80e3ad0ef39fb5d7efe052dbe6d
Morgan M <morgan.maxwell20@gmail.com>
2019-03-03 17:36:22 -0700

    save for later

--- public/js/index.js
+++ public/js/index.js
@@ -1,96 +1,98 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // Get references to page elements
-  var $exampleText = $("#example-text");
-  var $exampleDescription = $("#example-description");
   var $submitBtn = $("#submit");
   var $exampleList = $("#example-list");
+  var userId = "";
 
-  // The API object contains methods for each kind of request we'll make
-  var API = {
-    saveExample: function(example) {
-      return $.ajax({
-        headers: {
-          "Content-Type": "application/json"
-        },
-        type: "POST",
-        url: "api/examples",
-        data: JSON.stringify(example)
-      });
-    },
-    getExamples: function() {
-      return $.ajax({
-        url: "api/examples",
-        type: "GET"
-      });
-    },
-    deleteExample: function(id) {
-      return $.ajax({
-        url: "api/examples/" + id,
-        type: "DELETE"
-      });
-    }
+  // have to have the userId for the rest of the data to be retrieved from db properly
+  function getUserData() {
+    $.ajax("/api/user_data", {
+      method: "GET"
+    }).then(function (result) {
+      // store user's specific ID on a global variable.
+      userId = result.id;
+    });
+    return userId;
   };
 
-  // refreshExamples gets new examples from the db and repopulates the list
-  var refreshExamples = function() {
-    API.getExamples().then(function(data) {
-      var $examples = data.map(function(example) {
-        var $a = $("<a>")
-          .text(example.text)
-          .attr("href", "/example/" + example.id);
-
-        var $li = $("<li>")
-          .attr({
-            class: "list-group-item",
-            "data-id": example.id
-          })
-          .append($a);
-
-        var $button = $("<button>")
-          .addClass("btn btn-danger float-right delete")
-          .text("ｘ");
-
-        $li.append($button);
-
-        return $li;
+  // grab data from the database that matches the user's ID #
+  function getUserTodos() {
+    $.ajax("/api/todos/" + userId, {
+      method: "GET"
+    }).then(function (result) {
+      res.render("index", {todos: result}, function(err) {
+        if (err) throw err;
       });
-
-      $exampleList.empty();
-      $exampleList.append($examples);
     });
   };
+  // // The API object contains methods for each kind of request we'll make
+  // var API = {
+  //   userData: function() {
+  //     return $.ajax({
+  //       method: "GET",
+  //       url: "/api/user_data"
+  //     });
+  //   },
+  //   newTodo: function(example) {
+  //     return $.ajax({
+  //       headers: {
+  //         "Content-Type": "application/json"
+  //       },
+  //       type: "POST",
+  //       url: "api/examples",
+  //       data: JSON.stringify(example)
+  //     });
+  //   },
+  //   getTodos: function() {
+  //     return $.ajax({
+  //       url: "api/todos/" + userId,
+  //       type: "GET"
+  //     });
+  //   },
+  //   deleteExample: function(id) {
+  //     return $.ajax({
+  //       url: "api/examples/" + id,
+  //       type: "DELETE"
+  //     });
+  //   }
+  // };
+  // // refreshExamples gets new examples from the db and repopulates the list
+  // var refreshExamples = function() {
+  //   API.getExamples().then(function(data) {
+  //     var $examples = data.map(function(example) {
+  //       var $a = $("<a>")
+  //         .text(example.text)
+  //         .attr("href", "/example/" + example.id);
 
-  // handleFormSubmit is called whenever we submit a new example
-  // Save the new example to the db and refresh the list
-  var handleFormSubmit = function(event) {
-    event.preventDefault();
+  //       var $li = $("<li>")
+  //         .attr({
+  //           class: "list-group-item",
+  //           "data-id": example.id
+  //         })
+  //         .append($a);
 
-    var example = {
-      text: $exampleText.val().trim(),
-      description: $exampleDescription.val().trim()
-    };
+  //       var $button = $("<button>")
+  //         .addClass("btn btn-danger float-right delete")
+  //         .text("ｘ");
 
-    if (!(example.text && example.description)) {
-      alert("You must enter an example text and description!");
-      return;
-    }
+  //       $li.append($button);
 
-    API.saveExample(example).then(function() {
-      refreshExamples();
-    });
+  //       return $li;
+  //     });
 
-    $exampleText.val("");
-    $exampleDescription.val("");
-  };
+  //     $exampleList.empty();
+  //     $exampleList.append($examples);
+  //   });
+  // };
 
   // handleDeleteBtnClick is called when an example's delete button is clicked
   // Remove the example from the db and refresh the list
-  var handleDeleteBtnClick = function() {
+  var handleDeleteBtnClick = function () {
     var idToDelete = $(this)
       .parent()
       .attr("data-id");
 
-    API.deleteExample(idToDelete).then(function() {
+    API.deleteExample(idToDelete).then(function () {
       refreshExamples();
     });
   };
@@ -102,4 +104,8 @@ $(document).ready(function() {
   // open modal and allow collapsible checkboxes
   $(".modal").modal();
   $(".collapsible").collapsible();
+
+  // function calls
+  getUserData();
+  getUserTodos();
 });
