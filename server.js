260007caf6f831df1ea8b8192acfcc335f1797df
ibaveda <igorbaveda@gmail.com>
2019-03-06 10:51:44 -0700

    moved nodemailer to apiRoutes

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 var session = require("express-session");
 var passport = require("./config/passport");
-var nodemailer = require("nodemailer");
 
 var PORT = process.env.PORT || 8080;
 var db = require("./models");
@@ -53,39 +52,6 @@ db.sequelize.sync(syncOptions).then(function() {
       PORT
     );
   });
-});
-
-// NodeMailer functions
-let transporter = nodemailer.createTransport({
-  service: "gmail",
-  // host: "smtp.gmail.com",
-  // port: 465,
-  // // port: 465,
-  // secure: true, // true for 465, false for other ports
-  auth: {
-    user: "project2testuser@gmail.com", // generated ethereal user
-    pass: "Password!@#" // generated ethereal password
-  }
-  // maxMessages: 100 // need to specify amount allowed to send at once or emails fail to send
-});
-let mailOptions = {
-  // from: '"reminderApp :ghost:TESTING" <noreply@valenceservices.com',
-  from: '"Remindr" <noreply@remindr.com>',
-  replyto_name: "No Reply", //not defined properly but doesn't error code out
-  replyto_email: "noreply@remindr.com", //not defined properly but doesn't error code out
-  to: "project2testuser@gmail.com", // list of receivers
-  subject: "Reminder", // Subject line
-  text: 'Hello there, thanks for creating a reminder with us. We hope you enjoy using our App!', // plain text body
-  html: "<p>Hello there, thanks for creating a reminder with us. We hope you enjoy using our App!</p>", // html body
-  // attachDataUrls: link //company link
-};
-
-transporter.sendMail(mailOptions, (error, info) => {
-  if (error) {
-    return console.log(error, "error in sending email function");
-  }
-  console.log("Message sent: %s", info.messageId);
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 });
 
 module.exports = app;
